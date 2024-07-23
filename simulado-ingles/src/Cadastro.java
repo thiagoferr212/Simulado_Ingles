616201c353fa95f3fd53949f7f059357a5c15d63
Thiago Ferreira <thiagoferr212@gmail.com>
2024-07-23 12:10:04 -0300

    Criação das classes Cadastro e Usuario

---
+++ simulado-ingles/src/Cadastro.java
@@ -0,0 +1,30 @@
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import conexao.Conexao;
+import entity.Usuario;
+
+public class Cadastro {
+    
+    public void cadastrarUsuario(Usuario usuario){
+
+        String sql = "INSERT INTO t_usuario (nome, sobrenome, email, senha) VALUES (?, ?, ?, ?)";
+
+        PreparedStatement ps = null;
+
+        try {
+            ps = Conexao.getConexao().prepareStatement(sql);
+            ps.setString(1, usuario.getNome());
+            ps.setString(2, usuario.getSobrenome());
+            ps.setString(3, usuario.getEmail());
+            ps.setString(4, usuario.getSenha());
+
+            ps.execute();
+            ps.close();
+
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
